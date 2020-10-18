mokomull/stm32f4-discovery-signal-generator

commit 19b8cbc7d16a37a2208a6b6e683d2e65d9d62019

fix off-by-one error in string handling
We already don't include the '\n' in the length that UsbReadLine
returns, so everything's fine here.

src/main.rs
@@ -91,7 +91,7 @@ fn main() -> ! {
             let mut buffer = [0u8; 8];
             let len = usb_command.read_line(&mut buffer).await;
 
-            let value_bytes = buffer.iter().skip(1).take(len - 1);
+            let value_bytes = buffer.iter().skip(1).take(len);
             if value_bytes.clone().all(|&c| c >= b'0' && c <= b'9') {
                 let mut value: usize = 0;
                 for &c in value_bytes {

commit 9f5d86e457c419761edfd20a317710fe5dced293

fix length handling again
We *do* want to return the whole length of the command we received, and
then subtract the same number of bytes as we skipped...

This does not change behavior, but it makes the returned length much
more sane.

Fixes: 19b8cbc7d16a ("fix off-by-one error in string handling")

src/main.rs
@@ -91,7 +91,7 @@ fn main() -> ! {
             let mut buffer = [0u8; 8];
             let len = usb_command.read_line(&mut buffer).await;
 
-            let value_bytes = buffer.iter().skip(1).take(len);
+            let value_bytes = buffer.iter().skip(1).take(len - 1);
             if value_bytes.clone().all(|&c| c >= b'0' && c <= b'9') {
                 let mut value: usize = 0;
                 for &c in value_bytes {
@@ -327,7 +327,7 @@ impl<'a, 'b, T: usb_device::bus::UsbBus> Future for UsbReadLine<'a, 'b, T> {
                 {
                     me.target_buffer[i] = v;
                 }
-                return Poll::Ready(newline - 1);
+                return Poll::Ready(newline);
             }
         }
         Poll::Pending